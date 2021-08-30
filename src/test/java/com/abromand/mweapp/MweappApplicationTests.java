6c0ec835442e99bdc93a996e5ba89ecf9ff2867e
Amin Abromand <amin.abromand@metaproc.com>
2021-08-30 10:34:36 +0200

    initial commit

---
+++ src/test/java/com/abromand/mweapp/MweappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.abromand.mweapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MweappApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
