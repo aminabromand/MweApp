6950e7543ce590e563e407062cf69b62b9872bba
Amin Abromand <amin.abromand@metaproc.com>
2021-08-31 16:05:08 +0200

    enabled index reply

--- mweapp-web/src/main/java/com/abromand/mweapp/web/controller/IndexController.java
+++ mweapp-web/src/main/java/com/abromand/mweapp/web/controller/IndexController.java
@@ -1,12 +1,13 @@
 package com.abromand.mweapp.web.controller;
 
-import org.springframework.stereotype.Controller;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 public class IndexController {
 
-  @GetMapping
+  @GetMapping(produces = MediaType.TEXT_PLAIN_VALUE)
   public String index() {
     return "index";
   }
