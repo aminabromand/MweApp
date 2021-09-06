c095fd6d5fc544df344fd3a3623f71df3f390887
Amin Abromand <amin.abromand@metaproc.com>
2021-09-06 23:18:13 +0200

    base url config local vs prod and mwe user creator to be skipped if user exist

--- mweapp-gui/src/axios.js
+++ mweapp-gui/src/axios.js
@@ -1,11 +1,11 @@
 import axios from 'axios'
 
 // axios.defaults.baseURL = "http://localhost:8080"
-// axios.defaults.baseURL = import.meta.env.DEV ? "http://localhost:8080": "example.com";
+const myBaseUrl = process.env.VUE_APP_LOCAL_BASE_URL ? 'http://localhost:8080' : 'https://afternoon-mesa-12438.herokuapp.com'
 // axios.defaults.headers.common["Authorization"] = "Bearer " + localStorage.getItem("token");
 // axios.defaults.headers.common["Content-Type"] = "application/json";
 
 export const $axios = axios.create({
-  // baseURL: 'http://localhost:8080'
-  baseURL: 'https://afternoon-mesa-12438.herokuapp.com'
+  // baseURL: 'https://afternoon-mesa-12438.herokuapp.com'
+  baseURL: myBaseUrl
 })
--- mweapp-web/src/main/java/com/abromand/mweapp/web/security/MweUserCreator.java
+++ mweapp-web/src/main/java/com/abromand/mweapp/web/security/MweUserCreator.java
@@ -2,6 +2,7 @@ package com.abromand.mweapp.web.security;
 
 import com.abromand.mweapp.data.model.MweUser;
 import com.abromand.mweapp.data.repository.MweUserRepository;
+import java.util.Optional;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -24,21 +25,29 @@ public class MweUserCreator implements ApplicationListener<ApplicationReadyEvent
   @Override
   public void onApplicationEvent(ApplicationReadyEvent applicationReadyEvent) {
 
-    logger.info("running creator MweUserCreator");
+    logger.info("MweUserCreator running");
 
-    MweUser amin = new MweUser(1L, "Aminator", passwordEncoder.encode("abcdef9h"), "amin@abromand.com", true, false, "MWEler", "telenummer", "hauptstrasse", 3, "Software Engineer");
-    MweUser kappe = new MweUser(2L, "Kappe", passwordEncoder.encode("init101"), "kappert.d@web.de",  false, true, "MWEler", "telenummer", "hauptstrasse", 3, "Angestellter");
-    MweUser fame = new MweUser(3L, "Fame", passwordEncoder.encode("init101"), "felixblanke@gmail.com", false, false, "MWEler", "telenummer", "hauptstrasse", 3, "Angestellter");
-    MweUser poisch = new MweUser(4L, "Poisch", passwordEncoder.encode("init101"), "jenspoischbeg@gmail.com", false, false, "MWEler", "telenummer", "hauptstrasse", 3, "Angestellter");
-    MweUser jay = new MweUser(5L, "Jay", passwordEncoder.encode("init101"), "jensvictor@gmx.de", false, false, "MWEler", "telenummer", "hauptstrasse", 3, "Angestellter");
-    MweUser mo = new MweUser(6L, "Mo", passwordEncoder.encode("init101"), "moritz@reisers.net", false, false, "MWEler", "telenummer", "hauptstrasse", 3, "Angestellter");
-    MweUser porro = new MweUser(7L, "Porro", passwordEncoder.encode("init101"), "nicogoldmann@gmail.com", false, false, "MWEler", "telenummer", "hauptstrasse", 3, "Angestellter");
-    MweUser reen = new MweUser(8L, "Reen", passwordEncoder.encode("init101"), "ren.voss@gmail.com", false, false, "MWEler", "telenummer", "hauptstrasse", 3, "Angestellter");
-    MweUser bart = new MweUser(9L, "Bart", passwordEncoder.encode("init101"), "arnesalehian@gmailc.om", false, false, "MWEler", "telenummer", "hauptstrasse", 3, "Angestellter");
-    MweUser tobi = new MweUser(10L, "Tobi", passwordEncoder.encode("init101"), "tobiasschmitz@gmail.com", false, false, "MWEler", "telenummer", "hauptstrasse", 3, "Angestellter");
-    MweUser firstclaas = new MweUser(11L, "Firstclaas", passwordEncoder.encode("init101"), "claas.windheuser@gmail.com", false, false, "MWEler", "telenummer", "hauptstrasse", 3, "Angestellter");
-    MweUser pasen = new MweUser(12L, "Paisen", passwordEncoder.encode("init101"), "pascal.maas@gmx.de", false, false, "MWEler", "telenummer", "hauptstrasse", 3, "Angestellter");
-    MweUser freez = new MweUser(13L, "Freez", passwordEncoder.encode("init101"), "chr.freise@googlemail.com", false, false, "MWEler", "telenummer", "hauptstrasse", 3, "Angestellter");
+    Optional<MweUser> mweUserOptional = userRepository.findByUsername("aminator");
+    if(mweUserOptional.isPresent()) {
+      logger.info("MweUserCreator user already created");
+      return;
+    }
+
+    logger.info("MweUserCreator creating");
+
+    MweUser amin = new MweUser(1L, "aminator", passwordEncoder.encode("abcdef9h"), "amin@abromand.com", true, false, "MWEler", "telenummer", "hauptstrasse", 3, "Software Engineer");
+    MweUser kappe = new MweUser(2L, "kappe", passwordEncoder.encode("init101"), "kappert.d@web.de",  false, true, "MWEler", "telenummer", "hauptstrasse", 3, "Angestellter");
+    MweUser fame = new MweUser(3L, "fame", passwordEncoder.encode("init101"), "felixblanke@gmail.com", false, false, "MWEler", "telenummer", "hauptstrasse", 3, "Angestellter");
+    MweUser poisch = new MweUser(4L, "poisch", passwordEncoder.encode("init101"), "jenspoischbeg@gmail.com", false, false, "MWEler", "telenummer", "hauptstrasse", 3, "Angestellter");
+    MweUser jay = new MweUser(5L, "jay", passwordEncoder.encode("init101"), "jensvictor@gmx.de", false, false, "MWEler", "telenummer", "hauptstrasse", 3, "Angestellter");
+    MweUser mo = new MweUser(6L, "mo", passwordEncoder.encode("init101"), "moritz@reisers.net", false, false, "MWEler", "telenummer", "hauptstrasse", 3, "Angestellter");
+    MweUser porro = new MweUser(7L, "porro", passwordEncoder.encode("init101"), "nicogoldmann@gmail.com", false, false, "MWEler", "telenummer", "hauptstrasse", 3, "Angestellter");
+    MweUser reen = new MweUser(8L, "reen", passwordEncoder.encode("init101"), "ren.voss@gmail.com", false, false, "MWEler", "telenummer", "hauptstrasse", 3, "Angestellter");
+    MweUser bart = new MweUser(9L, "bart", passwordEncoder.encode("init101"), "arnesalehian@gmailc.om", false, false, "MWEler", "telenummer", "hauptstrasse", 3, "Angestellter");
+    MweUser tobi = new MweUser(10L, "tobi", passwordEncoder.encode("init101"), "tobiasschmitz@gmail.com", false, false, "MWEler", "telenummer", "hauptstrasse", 3, "Angestellter");
+    MweUser firstclaas = new MweUser(11L, "firstclaas", passwordEncoder.encode("init101"), "claas.windheuser@gmail.com", false, false, "MWEler", "telenummer", "hauptstrasse", 3, "Angestellter");
+    MweUser pasen = new MweUser(12L, "paisen", passwordEncoder.encode("init101"), "pascal.maas@gmx.de", false, false, "MWEler", "telenummer", "hauptstrasse", 3, "Angestellter");
+    MweUser freez = new MweUser(13L, "freez", passwordEncoder.encode("init101"), "chr.freise@googlemail.com", false, false, "MWEler", "telenummer", "hauptstrasse", 3, "Angestellter");
 
     userRepository.save(amin);
     userRepository.save(kappe);
@@ -53,6 +62,8 @@ public class MweUserCreator implements ApplicationListener<ApplicationReadyEvent
     userRepository.save(firstclaas);
     userRepository.save(pasen);
     userRepository.save(freez);
+
+    logger.info("MweUserCreator created");
 
   }
 }
